c7889f068be99044551619e462e8e1510b1453ae
pkshetlie <pierrick.pobelle@gmail.com>
2021-03-28 20:24:41 +0200

    first commit

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -11,11 +11,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractDashboardController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin")
      */
     public function index(): Response
     {
-        return parent::index();
+        // redirect to some CRUD controller
+        $routeBuilder = $this->get(AdminUrlGenerator::class);
+
+        return $this->redirect($routeBuilder->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // you can also redirect to different pages depending on the current user
+        if ('jane' === $this->getUser()->getUsername()) {
+            return $this->redirect('...');
+        }
+
+        // you can also render some template to display a proper Dashboard
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
