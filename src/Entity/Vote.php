5d4c4edf8c72e49b9ce6089bbf67b22869017dbc
pkshetlie <pierrick.pobelle@gmail.com>
2021-03-31 16:08:14 +0200

    debut de moderation + service discord

--- src/Entity/Vote.php
+++ src/Entity/Vote.php
@@ -4,12 +4,14 @@ namespace App\Entity;
 
 use App\Repository\VoteRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 /**
  * @ORM\Entity(repositoryClass=VoteRepository::class)
  */
 class Vote
 {
+    use TimestampableEntity;
     const KIND_UP = 100;
     const KIND_NEUTRAL = 0;
     const KIND_DOWN = 200;
@@ -36,6 +38,36 @@ class Vote
      * @ORM\Column(type="integer")
      */
     private $kind;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $FunFactor;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $Rhythm;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $Flow;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $PatternQuality;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $Readability;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $LevelQuality;
 
     public function getId(): ?int
     {
@@ -74,6 +106,78 @@ class Vote
     public function setKind(int $kind): self
     {
         $this->kind = $kind;
+
+        return $this;
+    }
+
+    public function getFunFactor(): ?float
+    {
+        return $this->FunFactor;
+    }
+
+    public function setFunFactor(?float $FunFactor): self
+    {
+        $this->FunFactor = $FunFactor;
+
+        return $this;
+    }
+
+    public function getRhythm(): ?float
+    {
+        return $this->Rhythm;
+    }
+
+    public function setRhythm(?float $Rhythm): self
+    {
+        $this->Rhythm = $Rhythm;
+
+        return $this;
+    }
+
+    public function getFlow(): ?float
+    {
+        return $this->Flow;
+    }
+
+    public function setFlow(?float $Flow): self
+    {
+        $this->Flow = $Flow;
+
+        return $this;
+    }
+
+    public function getPatternQuality(): ?float
+    {
+        return $this->PatternQuality;
+    }
+
+    public function setPatternQuality(?float $PatternQuality): self
+    {
+        $this->PatternQuality = $PatternQuality;
+
+        return $this;
+    }
+
+    public function getReadability(): ?float
+    {
+        return $this->Readability;
+    }
+
+    public function setReadability(float $Readability): self
+    {
+        $this->Readability = $Readability;
+
+        return $this;
+    }
+
+    public function getLevelQuality(): ?float
+    {
+        return $this->LevelQuality;
+    }
+
+    public function setLevelQuality(float $LevelQuality): self
+    {
+        $this->LevelQuality = $LevelQuality;
 
         return $this;
     }
