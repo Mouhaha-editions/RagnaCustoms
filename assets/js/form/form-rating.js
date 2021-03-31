038df4653a02800b7a3ebadce65fdca11b557769
pkshetlie <pierrick.pobelle@gmail.com>
2021-04-01 01:57:51 +0200

    debut de moderation + service discord

--- assets/js/form/form-rating.js
+++ assets/js/form/form-rating.js
@@ -1,54 +1,105 @@
-const app = require('../utils/core');
+// const app = require('../utils/core');
 
-app.querySelector('.form-rating', function (ratingInputs) {
-  for (const ratingInput of ratingInputs) {
-    const filledClass = 'filled',
-          ratingItems = Array.from(ratingInput.children);
+$(document).on('click', '.form-rating', function () {
+    let t = $(this);
+    let ratingItems = $(this).find('.rating');
 
-    const getStarsRating = function () {
-      let rating = 0;
-
-      for (const ratingItem of ratingItems) {
-        if (ratingItem.classList.contains(filledClass)) {
-          rating++;
+    let getStarsRating = function () {
+        let rating = 0;
+        for (let ratingItem of ratingItems) {
+            if ($(ratingItem).hasClass("filled")) {
+                rating++;
+            }
         }
-      }
-
-      return rating;
+        return rating;
     };
 
-    const setStarsRating = function () {
-      ratingInput.setAttribute('data-rating', getStarsRating());
-      var event = new Event('change');
-      ratingInput.dispatchEvent(event);
+    let setStarsRating = function () {
+        t.data('rating', getStarsRating());
+        t.trigger('change');
     };
 
     const fillStar = function (item) {
-      item.classList.add(filledClass);
+        $(item).addClass("filled");
     };
 
     const emptyStar = function (item) {
-      item.classList.remove(filledClass);
+        $(item).removeClass("filled");
     };
 
     const toggleStars = function () {
-      const itemIndex = ratingItems.indexOf(this);
 
-      for (let i = 0; i <= itemIndex; i++) {
-        fillStar(ratingItems[i]);
-      }
+        const itemIndex = $(this).data('id');
+        for (let i = 0; i <= itemIndex; i++) {
+            fillStar(ratingItems[i]);
+        }
 
-      for (let i = itemIndex + 1; i < ratingItems.length; i++) {
-        emptyStar(ratingItems[i]);
-      }
+        for (let i = itemIndex + 1; i < ratingItems.length; i++) {
+            emptyStar(ratingItems[i]);
+        }
 
-      setStarsRating();
+        setStarsRating();
     };
 
     for (const ratingItem of ratingItems) {
-      ratingItem.addEventListener('click', toggleStars);
+        $(ratingItem).on('click', toggleStars);
     }
 
     setStarsRating();
-  }
+
+
 });
+
+//
+// app.querySelector('.form-rating', function (ratingInputs) {
+//   for (const ratingInput of ratingInputs) {
+//     const filledClass = 'filled',
+//           ratingItems = Array.from(ratingInput.children);
+//
+//     const getStarsRating = function () {
+//       let rating = 0;
+//
+//       for (const ratingItem of ratingItems) {
+//         if (ratingItem.classList.contains(filledClass)) {
+//           rating++;
+//         }
+//       }
+//
+//       return rating;
+//     };
+//
+//     const setStarsRating = function () {
+//       ratingInput.setAttribute('data-rating', getStarsRating());
+//       var event = new Event('change');
+//       ratingInput.dispatchEvent(event);
+//     };
+//
+//     const fillStar = function (item) {
+//       item.classList.add(filledClass);
+//     };
+//
+//     const emptyStar = function (item) {
+//       item.classList.remove(filledClass);
+//     };
+//
+//     const toggleStars = function () {
+//       const itemIndex = ratingItems.indexOf(this);
+//
+//       for (let i = 0; i <= itemIndex; i++) {
+//         fillStar(ratingItems[i]);
+//       }
+//
+//       for (let i = itemIndex + 1; i < ratingItems.length; i++) {
+//         emptyStar(ratingItems[i]);
+//       }
+//
+//       setStarsRating();
+//     };
+//
+//     for (const ratingItem of ratingItems) {
+//       ratingItem.addEventListener('click', toggleStars);
+//     }
+//
+//     setStarsRating();
+//   }
+// });
