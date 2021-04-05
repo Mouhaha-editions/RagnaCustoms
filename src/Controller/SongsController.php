8c8a93d30cdd3f2406e17024161741f40fd2f673
pkshetlie <pierrick.pobelle@gmail.com>
2021-04-06 00:38:28 +0200

    add unique download tracking

--- src/Controller/SongsController.php
+++ src/Controller/SongsController.php
@@ -225,7 +225,7 @@ class SongsController extends AbstractController
 
             switch ($request->get('downloads_filter_order')) {
                 case 1:
-                    $qb->orderBy('if(s.countVotes = 0,-1,(s.totalVotes/s.countVotes))', 'DESC');
+                    $qb->orderBy('s.totalVotes/s.countVotes', 'DESC');
                     break;
                 case 2 :
                     $qb->orderBy('s.approximativeDuration', 'DESC');
