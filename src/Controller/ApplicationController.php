e028d6a6989e7a7e7a95ab81fca14fe4b01faffb
pkshetlie <pierrick.pobelle@gmail.com>
2021-04-05 20:23:06 +0200

    debut de moderation + service discord

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ApplicationController extends AbstractController
@@ -16,5 +18,13 @@ class ApplicationController extends AbstractController
         return $this->render('application/index.html.twig', [
             'controller_name' => 'ApplicationController',
         ]);
+    }
+    /**
+     * @Route("/locale/{locale}", name="change_locale")
+     */
+    public function changeLocale(Request $request, string $locale,SessionInterface $session)
+    {
+        $session->set('_locale', $locale);
+        return $this->redirect($request->headers->get('referer') );
     }
 }
