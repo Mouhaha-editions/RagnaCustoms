345a2576017d026fbac2fb3ebfe74fac500e424a
pkshetlie <pierrick.pobelle@gmail.com>
2021-04-15 18:41:46 +0200

    social

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use Abraham\TwitterOAuth\TwitterOAuth;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
