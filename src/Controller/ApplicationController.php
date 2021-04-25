fc24e993eec637f85932360721faa2a6990a5ed0
pkshetlie <pierrick.pobelle@gmail.com>
2021-04-25 23:54:05 +0200

    Twitch link and counter

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -23,6 +23,24 @@ class ApplicationController extends AbstractController
         ]);
     }
     /**
+     * @Route("/application/version", name="application_version")
+     */
+    public function version(): Response
+    {
+        return new Response('2.0.4');
+    }
+    /**
+     * @Route("/application/changelog", name="application_changelog")
+     */
+    public function changelog(): Response
+    {
+        return $this->render('application/changelog.html.twig', [
+            'controller_name' => 'ApplicationController',
+        ]);
+    }
+
+
+    /**
      * @Route("/application", name="application")
      */
     public function index(): Response
