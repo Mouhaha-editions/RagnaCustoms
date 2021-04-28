22cff36a415e12a4174a927d26b530b4e60b5ee0
pkshetlie <pierrick.pobelle@gmail.com>
2021-04-28 14:44:34 +0200

    global ranking

--- src/Controller/ScoreController.php
+++ src/Controller/ScoreController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Song;
+use App\Repository\ScoreRepository;
 use App\Repository\SongRepository;
+use Pkshetlie\PaginationBundle\Service\PaginationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,12 +20,28 @@ class ScoreController extends AbstractController
     {
         $songs = $songRepository->createQueryBuilder('s')
             ->where('s.moderated = true')
-            ->orderBy('s.name','ASC')->getQuery()->getResult();
+            ->orderBy('s.name', 'ASC')->getQuery()->getResult();
 
 
         return $this->render('score/index.html.twig', [
-            'songs'=>$songs
+            'songs' => $songs
         ]);
     }
 
+    /**
+     * @Route("/ranking/global", name="score_global_ranking")
+     */
+    public function globalRanking(Request $request, ScoreRepository $scoreRepository, PaginationService $paginationService): Response
+    {
+        $qb = $scoreRepository->createQueryBuilder('s')
+            ->select('u.username AS username, SUM(s.score)/1000 AS score, COUNT(s.song) AS count_song')
+            ->leftJoin('s.user', 'u')
+            ->groupBy('s.user')
+            ->orderBy('SUM(s.score)', 'DESC');
+        $scores = $paginationService->setDefaults(200)->process($qb, $request);
+
+        return $this->render('score/global_ranking.html.twig', [
+            'scores' => $scores
+        ]);
+    }
 }
