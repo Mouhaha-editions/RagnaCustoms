6023065eac7a03770f136d423e435ae854ca75a0
pkshetlie <pierrick.pobelle@gmail.com>
2021-05-19 22:34:31 +0200

    add feedback deposit

--- src/Form/SongFeedbackType.php
+++ src/Form/SongFeedbackType.php
@@ -2,8 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\SongDifficulty;
 use App\Entity\SongFeedback;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,15 +15,43 @@ class SongFeedbackType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        /** @var SongFeedback $entity */
+        $entity = $builder->getData();
         $builder
-            ->add('isPublic')
-            ->add('isAnonymous')
-            ->add('feedback')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('song')
-            ->add('songDifficulty')
-        ;
+            ->add('songDifficulty', EntityType::class, [
+                'class' => SongDifficulty::class,
+                'label' => "Level to feedback",
+                'query_builder' => function (EntityRepository $er) use ($entity) {
+                    return $er->createQueryBuilder('sd')
+                        ->where("sd.song = :song")
+                        ->setParameter("song", $entity->getSong());
+                },
+                "placeholder"=>"Global",
+                "required"=>false,
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('isPublic', null, [
+                "label" => "make it public",
+                "required"=>false,
+
+                "attr" => [
+                ]
+            ])
+            ->add('isAnonymous', null, [
+                "label" => "send it anonymously",
+                "required"=>false,
+                "attr" => [
+                ]
+            ])
+            ->add('feedback', TextareaType::class, [
+                "label" => "your feedback",
+                "required"=>true,
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
