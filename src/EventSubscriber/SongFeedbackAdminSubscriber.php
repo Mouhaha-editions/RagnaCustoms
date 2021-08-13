6d88c4bf4d1b076fdde9a486ab728c8309f89620
pkshetlie <pierrick.pobelle@gmail.com>
2021-08-13 11:26:35 +0200

    add some history on scores

---
+++ src/EventSubscriber/SongFeedbackAdminSubscriber.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\EventSubscriber;
+
+
+use App\Entity\SongFeedback;
+use App\Service\SongService;
+use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityUpdatedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\MailerInterface;
+
+class SongFeedbackAdminSubscriber implements EventSubscriberInterface
+{
+
+
+    /**
+     * @var SongService
+     */
+    private $songService;
+
+    public function __construct(SongService $songService)
+    {
+        $this->songService = $songService;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            AfterEntityUpdatedEvent::class => ['sendMail'],
+        ];
+    }
+
+    public function sendMail(AfterEntityUpdatedEvent $event)
+    {
+        /** @var SongFeedback $entity */
+        $entity = $event->getEntityInstance();
+        $song = $entity->getSong();
+        if($song->getUser()->getEnableEmailNotification() && $entity->getIsModerated()) {
+            $this->songService->newFeedbackForMapper($entity);
+        }
+    }
+}
