cac61c05d3db5214fbfc419d11ecd66e3a53a203
pkshetlie <pierrick.pobelle@gmail.com>
2021-08-18 16:04:13 +0200

    add some history on scores

--- src/Entity/ViewCounter.php
+++ src/Entity/ViewCounter.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use App\Repository\DownloadCounterRepository;
+use App\Repository\ViewCounterRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
 
---
+++ src/Service/StatisticService.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\DownloadCounter;
+use App\Entity\Song;
+use App\Entity\Utilisateur;
+use App\Entity\ViewCounter;
+use App\Entity\Vote;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\Security;
+
+class StatisticService
+{
+    private $security;
+    protected $requestStack;
+    protected $em;
+
+    public function __construct(Security $security, RequestStack $requestStack, EntityManagerInterface $em)
+    {
+        $this->security = $security;
+        $this->requestStack = $requestStack;
+        $this->em = $em;
+    }
+
+    public function getLastXDays($days = 30)
+    {
+        $first = (new DateTime())->modify("-" . $days . " days");
+        $days = [];
+        while ($first < new DateTime()) {
+            $days[] = $first->format('Y-m-d');
+            $first->modify("+1 day");
+        }
+        return $days;
+    }
+
+    public function getViewsLastXDays($days, Song $song)
+    {
+        $first = (new DateTime())->modify("-" . $days . " days");
+        $data = [];
+        while ($first < new DateTime()) {
+            $result = $this->em->getRepository(ViewCounter::class)->createQueryBuilder("d")
+                ->select('COUNT(d) AS nb')
+                ->where("d.song = :song")
+                ->andWhere("d.createdAt LIKE :date")
+                ->setParameter('song', $song)
+                ->setParameter('date', $first->format('Y-m-d')."%")
+                ->orderBy('d.song')
+                ->setFirstResult(0)->setMaxResults(1)
+                ->getQuery()->getOneOrNullResult();
+            $first->modify("+1 day");
+            $data[] = $result['nb'];
+        }
+        return $data;
+    }
+
+    public function getDownloadsLastXDays($days, Song $song)
+    {
+        $first = (new DateTime())->modify("-" . $days . " days");
+        $data = [];
+        while ($first < new DateTime()) {
+            $result = $this->em->getRepository(DownloadCounter::class)->createQueryBuilder("d")
+                ->select('COUNT(d) AS nb')
+                ->where("d.song = :song")
+                ->andWhere("d.createdAt LIKE :date")
+                ->setParameter('song', $song)
+                ->setParameter('date', $first->format('Y-m-d')."%")
+                ->orderBy('d.song')
+                ->setFirstResult(0)->setMaxResults(1)
+                ->getQuery()->getOneOrNullResult();
+            $first->modify("+1 day");
+
+            $data[] = $result['nb'];
+        }
+        return $data;
+    }
+}
+
