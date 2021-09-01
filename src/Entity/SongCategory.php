b255b0344b76579c5a3a3d585af9b8f2d4bfcb07
pkshetlie <pierrick.pobelle@gmail.com>
2021-09-01 10:36:08 +0200

    enable fake deletion

---
+++ src/Entity/SongCategory.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SongCategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SongCategoryRepository::class)
+ */
+class SongCategory
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $label;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isFeedbackable;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isReviewable;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Song::class, mappedBy="songCategories")
+     */
+    private $songs;
+
+    public function __construct()
+    {
+        $this->songs = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getIsFeedbackable(): ?bool
+    {
+        return $this->isFeedbackable;
+    }
+
+    public function setIsFeedbackable(bool $isFeedbackable): self
+    {
+        $this->isFeedbackable = $isFeedbackable;
+
+        return $this;
+    }
+
+    public function getIsReviewable(): ?bool
+    {
+        return $this->isReviewable;
+    }
+
+    public function setIsReviewable(bool $isReviewable): self
+    {
+        $this->isReviewable = $isReviewable;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Song[]
+     */
+    public function getSongs(): Collection
+    {
+        return $this->songs;
+    }
+
+    public function addSong(Song $song): self
+    {
+        if (!$this->songs->contains($song)) {
+            $this->songs[] = $song;
+            $song->addSongCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSong(Song $song): self
+    {
+        if ($this->songs->removeElement($song)) {
+            $song->removeSongCategory($this);
+        }
+
+        return $this;
+    }
+}
