170258d83a8be9fd2b4111d5314f6c2865493ac3
pkshetlie <pierrick.pobelle@gmail.com>
2021-09-09 20:23:53 +0200

    change on statistics axes

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,7 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 // import {Chart} from "chart.js";
+import 'lazysizes';
 
 'chart.js/dist/chart.min';
 // import './src/sass/main.scss';
