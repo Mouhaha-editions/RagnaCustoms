08f15b4580859d66c1ded69d432aad905aeed90c
pkshetlie <pierrick.pobelle@gmail.com>
2021-10-24 17:24:54 +0200

    change on statistics axes

--- src/Controller/PlaylistController.php
+++ src/Controller/PlaylistController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Playlist;
 use App\Repository\SongRepository;
+use Exception;
 use Pkshetlie\PaginationBundle\Service\PaginationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -40,10 +41,10 @@ class PlaylistController extends AbstractController
             return $this->redirectToRoute('home');
         }
         $qb = $songRepository->createQueryBuilder("s")
-            ->leftJoin("s.playlists",'playlist')
+            ->leftJoin("s.playlists", 'playlist')
             ->where('playlist = :playlist')
             ->setParameter("playlist", $playlist)
-        ->addOrderBy('s.name');
+            ->addOrderBy('s.name');
         if ($request->get('onclick_dl')) {
             $ids = $qb->select('s.id')->getQuery()->getArrayResult();
             return $this->redirect("ragnac://install/" . implode('-', array_map(function ($id) {
@@ -56,8 +57,53 @@ class PlaylistController extends AbstractController
         return $this->render('playlist/show.html.twig', [
             'playlist' => $playlist,
             'songs' => $songs,
-            "user"=>$playlist->getUser()
+            "user" => $playlist->getUser()
 
         ]);
+    }
+
+
+    /**
+     * @Route("/playlist/edit/{id}", name="playlist_edit")
+     * @param Request $request
+     * @param Playlist $playlist
+     * @param PaginationService $paginationService
+     * @param SongRepository $song
+     * @return Response
+     */
+    public function edit(Request $request, Playlist $playlist)
+    {
+        if (!$this->isGranted("ROLE_USER") || $this->getUser()->getId() !== $playlist->getUser()->getId()) {
+            $this->addFlash("danger", "You are not the owner of this playlist.");
+            return $this->redirectToRoute("playlist");
+        }
+        $this->addFlash("warning", "we are working on this feature, it comes soon ;)");
+
+        return $this->redirectToRoute("playlist");
+    }
+
+    /**
+     * @Route("/playlist/delete/{id}", name="playlist_delete")
+     * @param Request $request
+     * @param Playlist $playlist
+     * @param PaginationService $paginationService
+     * @param SongRepository $song
+     * @return Response
+     */
+    public function delete(Request $request, Playlist $playlist)
+    {
+        if (!$this->isGranted("ROLE_USER") || $this->getUser()->getId() !== $playlist->getUser()->getId()) {
+            $this->addFlash("danger", "You are not the owner of this playlist.");
+            return $this->redirectToRoute("playlist");
+        }
+        try {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($playlist);
+            $em->flush();
+            $this->addFlash("success", "Your playlist is deleted.");
+        }catch (Exception $e){
+            $this->addFlash("success", "Can't delete your playlist.");
+        }
+        return $this->redirectToRoute("playlist");
     }
 }
