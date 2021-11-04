c111edd31eedec64ded6f8a422eb64ddc94a5663
pkshetlie <pierrick.pobelle@gmail.com>
2021-11-05 00:14:35 +0100

    upgrade the sitemap to the next level paginate songs and add artists

--- assets/app.js
+++ assets/app.js
@@ -18,7 +18,46 @@ import 'lazysizes';
 // start the Stimulus application
 import 'bootstrap';
 import 'tarteaucitronjs';
+tarteaucitron.init({
+    // "privacyUrl": "", /* Privacy policy url */
 
+    "hashtag": "#tarteaucitron", /* Open the panel with this hashtag */
+    "cookieName": "tarteaucitron", /* Cookie name */
+
+    "orientation": "bottom", /* Banner position (top - bottom - middle - popup) */
+
+    "groupServices": true, /* Group services by category */
+
+    "showAlertSmall": true, /* Show the small banner on bottom right */
+    "cookieslist": true, /* Show the cookie list */
+
+    "showIcon": true, /* Show cookie icon to manage cookies */
+    // "iconSrc": "", /* Optionnal: URL or base64 encoded image */
+    "iconPosition": "BottomLeft", /* Position of the icon between BottomRight, BottomLeft, TopRight and TopLeft */
+
+    "adblocker": false, /* Show a Warning if an adblocker is detected */
+
+    "DenyAllCta": true, /* Show the deny all button */
+    "AcceptAllCta": true, /* Show the accept all button when highPrivacy on */
+    "highPrivacy": true, /* HIGHLY RECOMMANDED Disable auto consent */
+
+    "handleBrowserDNTRequest": false, /* If Do Not Track == 1, disallow all */
+
+    "removeCredit": true, /* Remove credit link */
+    "moreInfoLink": true, /* Show more info link */
+    "useExternalCss": false, /* If false, the tarteaucitron.css file will be loaded */
+
+    //"cookieDomain": ".my-multisite-domaine.fr", /* Shared cookie for subdomain website */
+
+    // "readmoreLink": "", /* Change the default readmore link pointing to tarteaucitron.io */
+
+    "mandatory": true /* Show a message about mandatory cookies */
+});
+
+tarteaucitron.user.gtagUa = 'G-0JR780RBXK';
+tarteaucitron.user.gtagMore = function () { /* add here your optionnal gtag() */
+};
+(tarteaucitron.job = tarteaucitron.job || []).push('gtag');
 $(document).on('click', 'form .rating', function () {
     let t = $(this);
     let ratingItems = $(this).parent().find('.rating');
