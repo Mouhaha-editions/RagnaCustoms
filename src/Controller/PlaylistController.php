8e6a3cb109a05de3a40cb7c812681ec97be0c07a
pkshetlie <pierrick.pobelle@gmail.com>
2021-11-15 19:45:56 +0100

    translation fix

--- src/Controller/PlaylistController.php
+++ src/Controller/PlaylistController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Playlist;
+use App\Entity\Song;
+use App\Form\PlaylistType;
+use App\Repository\PlaylistRepository;
 use App\Repository\SongRepository;
 use Exception;
 use Pkshetlie\PaginationBundle\Service\PaginationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,10 +35,12 @@ class PlaylistController extends AbstractController
      * @param Request $request
      * @param Playlist $playlist
      * @param PaginationService $paginationService
-     * @param SongRepository $song
+     * @param SongRepository $songRepository
      * @return Response
      */
-    public function show(Request $request, Playlist $playlist, PaginationService $paginationService, SongRepository $songRepository): Response
+
+    public function show(Request $request, Playlist $playlist, PaginationService $paginationService,
+                         SongRepository $songRepository): Response
     {
         if (!$playlist->getIsPublic()) {
             $this->addFlash("warning", "This playlist is not public");
@@ -51,8 +57,6 @@ class PlaylistController extends AbstractController
                     return array_pop($id);
                 }, $ids)));
         }
-
-
         $songs = $paginationService->setDefaults(72)->process($qb, $request);
         return $this->render('playlist/show.html.twig', [
             'playlist' => $playlist,
@@ -64,22 +68,61 @@ class PlaylistController extends AbstractController
 
 
     /**
+     * @Route("/playlist/remove", name="playlist_remove")
+     * @param Request $request
+     * @return Response
+     */
+    public function remove(Request $request,PlaylistRepository $playlistRepository, SongRepository $songRepository)
+    {
+        $playlist = $playlistRepository->find($request->get('playlist_id'));
+        if (!$this->isGranted("ROLE_USER") || $this->getUser()->getId() !== $playlist->getUser()->getId()) {
+            $this->addFlash("danger", "You are not the owner of this playlist.");
+            return new Response("",500);
+        }
+        $song = $songRepository->find($request->get('id'));
+        $playlist->removeSong($song);
+        $this->getDoctrine()->getManager()->flush();
+        return new JsonResponse();
+    }
+
+    /**
      * @Route("/playlist/edit/{id}", name="playlist_edit")
      * @param Request $request
      * @param Playlist $playlist
+     * @param SongRepository $songRepository
      * @param PaginationService $paginationService
-     * @param SongRepository $song
      * @return Response
      */
-    public function edit(Request $request, Playlist $playlist)
+    public function edit(Request $request, Playlist $playlist, SongRepository $songRepository, PaginationService $paginationService)
     {
         if (!$this->isGranted("ROLE_USER") || $this->getUser()->getId() !== $playlist->getUser()->getId()) {
             $this->addFlash("danger", "You are not the owner of this playlist.");
             return $this->redirectToRoute("playlist");
         }
-        $this->addFlash("warning", "we are working on this feature, it comes soon ;)");
+        $form = $this->createForm(PlaylistType::class, $playlist);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $this->getDoctrine()->getManager()->flush();
+            $this->addFlash('success',"Playlist edited!");
+        }
 
-        return $this->redirectToRoute("playlist");
+        $qb = $songRepository->createQueryBuilder("s")
+            ->leftJoin("s.playlists", 'playlist')
+            ->where('playlist = :playlist')
+            ->setParameter("playlist", $playlist)
+            ->addOrderBy('s.name');
+        if ($request->get('onclick_dl')) {
+            $ids = $qb->select('s.id')->getQuery()->getArrayResult();
+            return $this->redirect("ragnac://install/" . implode('-', array_map(function ($id) {
+                    return array_pop($id);
+                }, $ids)));
+        }
+        $songs = $paginationService->setDefaults(72)->process($qb, $request);
+        return $this->render('playlist/edit.html.twig', [
+            'form' => $form->createView(),
+            'playlist' => $playlist,
+            'songs' => $songs
+        ]);
     }
 
     /**
@@ -101,7 +144,7 @@ class PlaylistController extends AbstractController
             $em->remove($playlist);
             $em->flush();
             $this->addFlash("success", "Your playlist is deleted.");
-        }catch (Exception $e){
+        } catch (Exception $e) {
             $this->addFlash("success", "Can't delete your playlist.");
         }
         return $this->redirectToRoute("playlist");
---
+++ src/Form/PlaylistType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Playlist;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlaylistType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('label', null, ["attr"=>["class"=>"form-control"]])
+            ->add('isPublic', null, ["attr"=>["class"=>""]])
+            ->add('description', null, ["attr"=>["class"=>"form-control"]])
+            ->add('save', SubmitType::class, ["attr"=>["class"=>"btn btn-info"]])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Playlist::class,
+        ]);
+    }
+}
