a48c66b5b2f13455acceef624924100ada6e90b3
pkshetlie <pierrick.pobelle@gmail.com>
2022-01-13 21:54:26 +0100

    fix slug issue on song deletion

--- assets/app.js
+++ assets/app.js
@@ -125,9 +125,34 @@ let hashtag = window.location.hash;
 function loadForm(content) {
     $("#form-edit").html(content);
     $("#form-edit form").on('submit', function () {
-        $("#form-edit").html("<div class=\"popup-box-actions white full void\">Sending your form</div>");
+        $("#form-edit").html("<div class=\"popup-box-actions white full void\">Sending your form, please wait ... </div> " +
+            "<div class='progress-container'><div class='progress'></div></div>");
         let tt = $(this);
+
         $.ajax({
+            xhr: function () {
+                let xhr = new window.XMLHttpRequest();
+                xhr.upload.addEventListener("progress", function (evt) {
+                    if (evt.lengthComputable) {
+                        let percentComplete = evt.loaded / evt.total;
+                        $('.progress').css({
+                            width: percentComplete * 100 + '%'
+                        });
+                        if (percentComplete === 1) {
+                            $('.progress').addClass('hide');
+                        }
+                    }
+                }, false);
+                xhr.addEventListener("progress", function (evt) {
+                    if (evt.lengthComputable) {
+                        let percentComplete = evt.loaded / evt.total;
+                        $('.progress').css({
+                            width: percentComplete * 100 + '%'
+                        });
+                    }
+                }, false);
+                return xhr;
+            },
             url: tt.attr('action'),
             data: new FormData(this),
             type: tt.attr('method'),
@@ -141,12 +166,13 @@ function loadForm(content) {
                     $("#form-edit").html(data.response);
                     loadForm(data.response);
                 } else {
-                    t.closest(t.data('replace-selector')).html(data.response);
+                    tt.closest(tt.data('replace-selector')).html(data.response);
                     $(tt).closest(".modal").modal('hide');
                 }
-
             }
         });
+
+
         $("#form-review").html("<div class=\"popup-box-actions white full void\">Sending your form</div>");
         return false;
     });
