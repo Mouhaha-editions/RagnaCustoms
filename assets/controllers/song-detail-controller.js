ecee2534d505a2de6a207064d587be03e643b43a
pkshetlie <pierrick.pobelle@gmail.com>
2022-02-15 17:34:21 +0100

    ajout de stimulus & swup, ca a l'air fun

--- assets/controllers/song-detail-controller.js
+++ assets/controllers/song-detail-controller.js
@@ -1,11 +1,15 @@
 import { Controller } from '@hotwired/stimulus';
+import { average } from 'color.js'
 
 export default class extends Controller {
-    static targets = ['name', 'output']
-
-    greet() {
-        console.log("greet"); // Swup instance
-
-        this.outputTarget.textContent = `Hello, ${this.nameTarget.value}!`
+    static targets = ['img', 'background']
+    connect() {
+        average(this.imgTarget.src, { amount: 1 }).then(color => {
+            console.log(color) // [241, 221, 63]
+            this.backgroundTarget.style = " background: radial-gradient(100% 100% at 0% 0%, rgba("+color[0]+", "+color[1]+", "+color[2]+", 0.2) 0%, rgba(0, 0, 0, 0) 100%), #2B2B2B;";
+        })
+    }
+    back() {
+        history.back(); // Swup instance
     }
 }
