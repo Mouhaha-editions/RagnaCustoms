09af6cdd6db2aed029863978036f4c19f5884b17
pkshetlie <pierrick.pobelle@gmail.com>
2022-02-15 17:47:27 +0100

    ajout de stimulus & swup, ca a l'air fun

--- assets/controllers/song-detail-controller.js
+++ assets/controllers/song-detail-controller.js
@@ -1,15 +1,16 @@
 import { Controller } from '@hotwired/stimulus';
 import { average } from 'color.js'
+import  'jquery'
 
 export default class extends Controller {
     static targets = ['img', 'background']
     connect() {
         average(this.imgTarget.src, { amount: 1 }).then(color => {
-            console.log(color) // [241, 221, 63]
-            this.backgroundTarget.style = " background: radial-gradient(100% 100% at 0% 0%, rgba("+color[0]+", "+color[1]+", "+color[2]+", 0.2) 0%, rgba(0, 0, 0, 0) 100%), #2B2B2B;";
-        })
+            $("body").attr('style', " background: radial-gradient(100% 100% at 0% 0%, rgba("+color[0]+", "+color[1]+", "+color[2]+", 0.2) 0%, rgba(0, 0, 0, 0) 100%), #2B2B2B;");
+        });
+            $(".back-button").attr('href', document.referrer);
     }
     back() {
-        history.back(); // Swup instance
+       if($(".back-button").attr('href') === undefined || $(".back-button").attr('href') === "" ) history.back();// Swup instance
     }
 }
