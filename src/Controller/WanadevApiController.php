d8c5d8c233214466541f7ac2072a7b8e354b23d8
pkshetlie <pierrick.pobelle@gmail.com>
2022-04-01 16:06:38 +0200

    fix for leaderboard crash

--- src/Controller/WanadevApiController.php
+++ src/Controller/WanadevApiController.php
@@ -39,10 +39,10 @@ class WanadevApiController extends AbstractController
     /**
      * @Route("/wanapi/score/{apiKey}/{osef}-{hash}", name="wd_api_score_simple_get",methods={"GET","POST"})
      */
-    public function scoreSimple(Request $request, string $apiKey, string $hash, SongDifficultyRepository $songDifficultyRepository, UtilisateurRepository $utilisateurRepository, ScoreService $scoreService, RankedScoresRepository $rankedScoresRepository, LoggerInterface $logger, ScoreRepository $scoreRepository, $onlyMe = true): Response
+    public function scoreSimple(Request $request, string $apiKey, string $hash, SongDifficultyRepository $songDifficultyRepository, UtilisateurRepository $utilisateurRepository, ScoreService $scoreService, RankedScoresRepository $rankedScoresRepository, LoggerInterface $logger, ScoreRepository $scoreRepository,ScoreHistoryRepository $scoreHistoryRepository, $onlyMe = true): Response
     {
 
-        return $this->score($request, $apiKey, $hash, $songDifficultyRepository, $utilisateurRepository, $scoreService, $rankedScoresRepository, $logger, $scoreRepository, false);
+        return $this->score($request, $apiKey, $hash, $songDifficultyRepository, $utilisateurRepository, $scoreService, $rankedScoresRepository, $logger, $scoreRepository, $scoreHistoryRepository,false);
     }
 
     /**
