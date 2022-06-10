1ed592f97604bdbc0ad1106e30294d81d2f4c77b
pkshetlie <pierrick.pobelle@gmail.com>
2022-06-10 12:17:37 +0200

    push notification

--- public/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('push', (e) => {
-    const data = e.data.json();
-    self.registration.showNotification(data.title, {
-        body: data.body,
-        icon: data.icon,
-    });
-});
