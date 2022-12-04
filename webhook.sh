01ef39a2699094a5e2bf80d94dccbbfa8a4ef7fc
Rhokapa <pierrick.pobelle@gmail.com>
2022-12-04 10:34:55 +0100

    changes on response when downloading

--- webhook.sh
+++ webhook.sh
@@ -1,6 +1,6 @@
-composer install
+#composer install
 #yarn install
-yarn encore production
-php bin/console doctrine:migrations:migrate --no-interaction
+#yarn encore production
+#php bin/console doctrine:migrations:migrate --no-interaction
 php bin/console c:c --env=prod
 #php bin/console opti:covers
