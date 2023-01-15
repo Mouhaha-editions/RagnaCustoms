fa496261607e6213dd7ad87430f461df899bf884
Rhokapa <pierrick.pobelle@gmail.com>
2023-01-15 12:43:44 +0100

    allow apikey reset

--- webhook.sh
+++ webhook.sh
@@ -1,6 +1,6 @@
 #composer install
 #yarn install
-yarn encore production
+#yarn encore production
 php bin/console doctrine:migrations:migrate --no-interaction
 php bin/console c:c --env=prod
 #php bin/console opti:covers
