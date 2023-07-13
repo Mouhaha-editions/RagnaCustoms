1a4f9b6ee017e3322d49cbe24b2045a924d22663
Rhokapa <pierrick.pobelle@gmail.com>
2023-07-13 18:38:20 +0200

    new button, updates and fixes

--- assets/controllers/mapper-profile-controller.js
+++ assets/controllers/mapper-profile-controller.js
@@ -19,6 +19,15 @@ export default class extends Controller {
         });
         $('.select2entity[data-autostart="true"]').select2entity();
         $('.select2').select2();
+        $('.circle').each(function(){
+            let items =$(this).find('.menuItem');
+
+            for(let i = 0, l = items.length; i < l; i++) {
+                items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+
+                items[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+            }
+        })
     }
 
     disconnect() {
--- assets/controllers/song-upload-v2-controller.js
+++ assets/controllers/song-upload-v2-controller.js
@@ -24,6 +24,15 @@ export default class extends Controller {
                 $(file.previewTemplate).find(" .dz-image").css({'background':"url("+resp.cover+")",'background-size':'cover'})
             }
         });
+        $('.circle').each(function(){
+            let items =$(this).find('.menuItem');
+
+            for(let i = 0, l = items.length; i < l; i++) {
+                items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+
+                items[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+            }
+        })
     }
 
 
