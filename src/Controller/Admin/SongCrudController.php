eb638aff334d6c6b711f3c8d51816bc995f97ded
Rhokapa <pierrick.pobelle@gmail.com>
2023-07-18 22:21:31 +0200

    can now remove up/down votes

--- src/Controller/Admin/SongCrudController.php
+++ src/Controller/Admin/SongCrudController.php
@@ -5,10 +5,13 @@ namespace App\Controller\Admin;
 use App\Entity\Song;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
 
 class SongCrudController extends AbstractCrudController
 {
@@ -23,15 +26,17 @@ class SongCrudController extends AbstractCrudController
         return $crud;
     }
 
-//    public function configureFields(string $pageName): iterable
-//    {
-//        return [
-////            IdField::new('id'),
-//            TextField::new('name'),
-////            TextField::new('user'),
-//            BooleanField::new('isModerated'),
-//            TextEditorField::new('description'),
-//        ];
-//    }
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('name'),
+            TextField::new('user', 'Mapper'),
+            BooleanField::new('isModerated'),
+            ChoiceField::new('bestPlatform')->setChoices(['vr'=>'0','flat'=>'1']),
+            BooleanField::new('isWip'),
+            TextEditorField::new('description'),
+        ];
+    }
 
 }
