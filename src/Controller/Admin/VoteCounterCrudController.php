678961f375006f190d5d87b4c7a0d344b796d432
Rhokapa <pierrick.pobelle@gmail.com>
2023-07-22 11:03:55 +0200

    fix review deletion

--- src/Controller/Admin/VoteCounterCrudController.php
+++ src/Controller/Admin/VoteCounterCrudController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\VoteCounter;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
@@ -16,6 +18,19 @@ class VoteCounterCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return VoteCounter::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+
+            // ...
+            // (the same permission is granted to the action on all pages)
+            ->setPermission(Action::EDIT, 'ROLE_ADMIN')
+            ->setPermission(Action::DELETE, 'ROLE_MODERATOR')
+            ->setPermission(Action::BATCH_DELETE, 'ROLE_ADMIN')
+            // you can set permissions for built-in actions in the same way
+            ->setPermission(Action::NEW, 'ROLE_ADMIN');
     }
 
     public function configureCrud(Crud $crud): Crud
