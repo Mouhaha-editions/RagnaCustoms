fcf7ced143d4922b86ee807a87e807b01d74a3a6
Rhokapa <pierrick.pobelle@gmail.com>
2023-08-21 18:30:29 +0200

    Need to play to leave a feedback

--- src/Service/MembershipService.php
+++ src/Service/MembershipService.php
@@ -18,16 +18,16 @@ class MembershipService
     public function  displayUsername(Utilisateur $user, bool $withPrefix = true)
     {
         if($this->grantedService->isGranted($user,'ROLE_PREMIUM_LVL2')){
-            return "<span style='color:".$user->getUsernameColor()."'>".($withPrefix ? "<i data-toggle='tooltip' title='Premium member' class='fas fa-gavel'></i> ":'').$user->getUsername()."</span>";
+            return "<span title=\"Premium member\" style='color:".$user->getUsernameColor()."'>".($withPrefix ? "<i data-toggle='tooltip' title='Premium member' class='fas fa-gavel'></i> ":'').$user->getUsername()."</span>";
         }else{
             return "<span style='color:#ffffff'>".$user->getUsername()."</span>";
         }
     }
 
-    public function  displayMappername(Utilisateur $user)
+    public function  displayMappername(Utilisateur $user, bool $withPrefix = true)
     {
         if($this->grantedService->isGranted($user,'ROLE_PREMIUM_LVL2')){
-            return "<span style='color:".$user->getUsernameColor()."'><i data-toggle='tooltip' title='Premium member' class='fas fa-gavel'></i> ".($user->getMapperName()??$user->getUsername())."</span>";
+            return "<span title='Premium member' style='color:".$user->getUsernameColor()."'>".($withPrefix ? "<i data-toggle='tooltip' title='Premium member' class='fas fa-gavel'></i> ":'').($user->getMapperName()??$user->getUsername())."</span>";
         }else{
             return "<span>".($user->getMapperName()??$user->getUsername())."</span>";
         }
