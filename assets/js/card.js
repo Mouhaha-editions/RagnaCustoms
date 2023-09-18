bcefcbb12032b21026b0ccc4b6a9ac7f2b1dbcc5
Rhokapa <pierrick.pobelle@gmail.com>
2023-09-18 21:46:48 +0200

    des paillettes dans la vie !

--- assets/js/card.js
+++ assets/js/card.js
@@ -10,9 +10,19 @@ $(document).on('mousemove', '.card', function (e) {
     let rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2);
     let rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2);
 
+    if($(this).find('.glow').length > 0 ) {
+        $(this).find('.glow')
+        $(this).find('.glow').css({'top': (clientY - top - clientWidth)+"px",'left': (clientX - left - clientHeight)+"px"})
+    }else{
+        $(this).append('<div class="glow"></div>');
+        $(this).find('.glow').css({'width': (clientWidth*2) +"px",'height': (clientHeight*2 )+"px"})
+    }
+
     $(this).css({transform: `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`});
 });
 
 $(document).on('mouseleave', '.card', function (e) {
     $(this).css({transform: `perspective(${e.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`});
+    $(this).find('.glow').remove();
+
 });
