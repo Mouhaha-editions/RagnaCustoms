af1cf4c77ae99d080446173b8f986960b4f3fcf6
Rhokapa <pierrick.pobelle@gmail.com>
2023-12-11 20:26:52 +0100

    fixes

--- src/Controller/Admin/SongCrudController.php
+++ src/Controller/Admin/SongCrudController.php
@@ -14,6 +14,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -38,19 +39,21 @@ class SongCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id')->hideOnForm(),
-            TextField::new('name'),
-            AssociationField::new('mappers'),
-            AssociationField::new('categoryTags'),
-            TextEditorField::new('description'),
-            TextField::new('youtubeLink'),
-            BooleanField::new('isConverted'),
+            FormField::addPanel('Base')->setCssClass('col-6'),
+            IdField::new('id')->hideOnForm()->setColumns('col-12'),
+            TextField::new('name')->setColumns('col-12'),
+            AssociationField::new('mappers')->setColumns('col-12'),
+            AssociationField::new('categoryTags')->setColumns('col-12'),
+            TextEditorField::new('description')->setColumns('col-12'),
+            TextField::new('youtubeLink')->setColumns('col-12'),
+            FormField::addPanel('Base')->setCssClass('col-6'),
+            BooleanField::new('isConverted')->setColumns('col-12'),
             ChoiceField::new('bestPlatform')
-                ->setChoices(['VR'=>'0','VOT'=>'1'])->allowMultipleChoices()->renderExpanded(),
-            BooleanField::new('isWip'),
-            DateTimeField::new('lastDateUpload'),
-            DateTimeField::new('programmationDate'),
-            BooleanField::new('isModerated'),
+                ->setChoices(['VR'=>'0','VOT'=>'1'])->allowMultipleChoices()->renderExpanded()->setColumns('col-12'),
+            BooleanField::new('isWip')->setColumns('col-12'),
+            DateTimeField::new('lastDateUpload')->setColumns('col-12'),
+            DateTimeField::new('programmationDate')->setColumns('col-12'),
+            BooleanField::new('isModerated')->setColumns('col-12'),
         ];
     }
 
