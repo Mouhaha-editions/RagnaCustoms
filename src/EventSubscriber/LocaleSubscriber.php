0b83ea2cce7cb211cd17af1819b02cf0e4c5b29b
Rhokapa <pierrick.pobelle@gmail.com>
2024-01-16 18:32:28 +0100

    bug fixes

--- src/EventSubscriber/LocaleSubscriber.php
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -38,12 +38,12 @@ class LocaleSubscriber implements EventSubscriberInterface
         }
 
         // try to see if the locale has been set as a _locale routing parameter
-        if ($locale = $request->attributes->get('_locale')) {
-            $request->getSession()->set('_locale', $locale);
-        } else {
-            // if no explicit locale has been set on this request, use one from the session
-            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
-        }
+        // if ($locale = $request->attributes->get('_locale')) {
+        //     $request->getSession()->set('_locale', $locale);
+        // } else {
+        //     // if no explicit locale has been set on this request, use one from the session
+        //     $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        // }
     }
 
     public function onCheckPassport(CheckPassportEvent $event): void
