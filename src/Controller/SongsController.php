16824869a6d9e9e264fc277297296d3cc08ce5fa
Rhokapa <pierrick.pobelle@gmail.com>
2024-05-26 12:56:55 +0200

    fix

--- src/Controller/SongsController.php
+++ src/Controller/SongsController.php
@@ -371,6 +371,15 @@ class SongsController extends AbstractController
 
     private function cleanName(?string $getName)
     {
+        $cyr  = array('а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у',
+            'ф','х','ц','ч','ш','щ','ъ', 'ы','ь', 'э', 'ю','я','А','Б','В','Г','Д','Е','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У',
+            'Ф','Х','Ц','Ч','Ш','Щ','Ъ', 'Ы','Ь', 'Э', 'Ю','Я' );
+        $lat = array( 'a','b','v','g','d','e','io','zh','z','i','y','k','l','m','n','o','p','r','s','t','u',
+            'f' ,'h' ,'ts' ,'ch','sh' ,'sht' ,'a', 'i', 'y', 'e' ,'yu' ,'ya','A','B','V','G','D','E','Zh',
+            'Z','I','Y','K','L','M','N','O','P','R','S','T','U',
+            'F' ,'H' ,'Ts' ,'Ch','Sh' ,'Sht' ,'A' ,'Y' ,'Yu' ,'Ya' );
+
+        $getName = str_replace($cyr, $lat, $getName);
         return preg_replace('/[^a-zA-Z]/i', '', $getName);
     }
 
