fceff386ad0ed81eb11822b65178b901842ee0b5
Rhokapa <pierrick.pobelle@gmail.com>
2024-10-31 19:47:25 +0100

    fix on date

--- src/Entity/RankedScores.php
+++ src/Entity/RankedScores.php
@@ -68,7 +68,7 @@ class RankedScores
 
     public function getTimeAgoShort()
     {
-      return StatisticService::dateDisplayedShort();
+      return StatisticService::dateDisplayedShort($this->updatedAt);
     }
 
     public function getPlateform(): ?string
